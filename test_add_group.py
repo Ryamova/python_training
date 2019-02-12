d64b4ab1fb6c4af8536e608b76d62132a4b6ffcb
Elena Ryamova <dgokonda2@bk.ru>
2019-02-12 14:16:56 +0300

    Добавление комментариев с помощью #

--- test_add_group.py
+++ test_add_group.py
@@ -12,7 +12,9 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
+        # open home page (открываем гл.страницы)
         wd.get("http://localhost/addressbook/group.php")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -21,8 +23,11 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page (открытие страницы со списком групп)
         wd.find_element_by_name("new").click()
+        # init group creation (создаем новую группу)
         wd.find_element_by_name("group_name").click()
+        # fill group form
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("gdkfhnv")
         wd.find_element_by_name("group_header").click()
@@ -31,9 +36,12 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("fhghjjss")
+        # submit group creation (нажимаем кнопку сабмит)
         wd.find_element_by_name("submit").click()
+        # return to groups page
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("groups").click()
+        # logaut 
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
