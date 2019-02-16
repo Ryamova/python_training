e7749407738431fc4dfb77a1372d1fd6c907f5be
Elena Ryamova <dgokonda2@bk.ru>
2019-02-16 18:23:42 +0300

    Небольшое улучшение читаемости тестов

--- test_add_group.py
+++ test_add_group.py
@@ -13,20 +13,14 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="gdkfhnv", header="fhhjdkjc", footer="fhghjjss"))
-        self.return_to_groups_page(wd)
         self.logaut(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logaut(wd)
 
     def logaut(self, wd):
@@ -39,6 +33,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         wd.find_element_by_name("group_name").click()
         # fill group form
         wd.find_element_by_name("group_name").clear()
@@ -51,11 +46,13 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation (нажимаем кнопку сабмит)
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_name("new").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
