e89d8a4f4c6b4eb18e1f67b17a48f53cd98f61ca
Elena Ryamova <dgokonda2@bk.ru>
2019-02-16 18:46:20 +0300

    Небольшое улучшение читаемости тестов (без wd)

--- test_add_group.py
+++ test_add_group.py
@@ -13,27 +13,30 @@ class TestAddGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="gdkfhnv", header="fhhjdkjc", footer="fhghjjss"))
-        self.logaut(wd)
+        self.login( username="admin", password="secret")
+        self.create_group( Group(name="gdkfhnv", header="fhhjdkjc", footer="fhghjjss"))
+        self.logaut()
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.logaut(wd)
+        self.login( username="admin", password="secret")
+        self.create_group( Group(name="", header="", footer=""))
+        self.logaut()
 
-    def logaut(self, wd):
+    def logaut(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
 
-    def return_to_groups_page(self, wd):
+    def return_to_groups_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, group):
-        self.open_groups_page(wd)
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
         wd.find_element_by_name("group_name").click()
         # fill group form
         wd.find_element_by_name("group_name").clear()
@@ -46,13 +49,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation (нажимаем кнопку сабмит)
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page(wd)
+        self.return_to_groups_page()
 
-    def open_groups_page(self, wd):
+    def open_groups_page(self):
+        wd = self.wd
         wd.find_element_by_name("new").click()
 
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -62,15 +67,18 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
     def is_element_present(self, how, what):
+        wd = self.wd
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
+        wd = self.wd
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
