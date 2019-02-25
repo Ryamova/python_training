eece51a4408b798d4a3c4f4edea6f2eca0443fb9
Ryamova <dgokonda2@bk.ru>
2019-02-25 22:26:07 +0300

    Конец создания теста (удаление группы)

--- fixture/group.py
+++ fixture/group.py
@@ -9,11 +9,11 @@ class GroupHelper:
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
+        self.open_group_create()
         wd.find_element_by_name("group_name").click()
         # fill group form
         wd.find_element_by_name("group_name").clear()
@@ -28,6 +28,19 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        #select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
+        wd = self.app.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+    def open_group_create(self):
         wd = self.app.wd
         wd.find_element_by_name("new").click()
