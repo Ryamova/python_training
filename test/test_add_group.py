431bd4cb3f339faf5cfe0ac443083f297b40718e
Ryamova <dgokonda2@bk.ru>
2019-03-03 19:19:39 +0300

    Повышение "интеллектуальности" функций login и logout

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,6 @@ from model.group import Group
 def test_add_group(app):
  app.group.create(Group(name="gdkfhnv", header="fhhjdkjc", footer="fhghjjss"))
 
-
 def test_add_empty_group(app):
  app.group.create(Group(name="", header="", footer=""))
 
